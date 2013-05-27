c36909cc44e3d8e445768b858ef154c5e6b8bf50
Efexis <efexlike@gmail.com>
2013-05-27 17:39:05 +0200

    Создаем модель AuthModel с методом проверки юзера

---
+++ application/models/AuthModel.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Control Panel
+ *
+ * Copyright (c) 2013 Efex
+ *
+ * Панель управления для серверов WoW
+ *
+ * @author   Efex
+ * @license  GNU General Public License
+ * @link     https://github.com/Efexis/ControlPanel/
+ * @version  0.0.1
+ */
+ 
+class AuthModel extends Model {
+
+    public $error_message;
+
+    public function isValidUser() {
+        if ( isset($_POST['user'], $_POST['pass']) ) {
+            if ( !empty($_POST['user']) && !empty($_POST['pass']) ) {
+                if ( preg_match("/^[a-zA-Z0-9]+$/", $_POST['user']) &&
+                     preg_match("/^\w*$/", $_POST['pass'])) {
+                    $user = $_POST['user'];
+                    $pass = $_POST['pass'];
+                    $sql = "SELECT `a`.`id`, `a`.`username`, `aa`.`gmlevel`
+                            FROM `".$this->config['db.auth']."`.`account` AS `a`
+                            LEFT JOIN `".$this->config['db.auth']."`.`account_access` AS `aa` ON `aa`.`id` = `a`.`id`
+                            WHERE `a`.`sha_pass_hash` = SHA1(CONCAT(UPPER('".$user."'),':',UPPER('".$pass."'))) AND
+                                    `aa`.`gmlevel` >= " . $this->config['cp.gmlevel'];
+                    $result = $this->db['auth']->query($sql);
+
+                    if ( $result->rowCount() == 1 ) {
+                        $_SESSION['session'] = session_id();
+                        $_SESSION['user'] = $user;
+                        header('Location: ?route=index/main');
+                        exit;
+                    } else {
+                        $this->error_message = 'У вас недостаточно прав для входа';
+                    }
+                } else {
+                    $this->error_message = 'Введены некорректные данные';
+                }
+            } else {
+                $this->error_message = 'Заполните пожалуйста форму';
+            }
+        }
+    }
+}
+?>
