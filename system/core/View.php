af31023eb058870e879afb392f3e1b0f920764f2
Efexis <efexlike@gmail.com>
2013-05-27 17:12:21 +0200

    Создаем основные классы Model и View

---
+++ system/core/View.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Control Panel
+ *
+ * Copyright (c) 2013 Efex
+ *
+ * Панель управления для серверов WoW
+ *
+ * @author   Efex
+ * @license  GNU General Public License
+ * @link     https://github.com/Efexis/ControlPanel/
+ * @version  0.0.1
+ */
+
+class View {}
+?>
