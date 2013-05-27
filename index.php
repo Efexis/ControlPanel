dfb4204dd1999807df2b7d113641f1abc28aed71
Efexis <efexlike@gmail.com>
2013-05-27 17:09:05 +0200

    Описываем автозагрузчик(autoloader)

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Control Panel
+ *
+ * Copyright (c) 2013 Efex
+ *
+ * Панель управления для серверов WoW
+ *
+ * @author   Efex
+ * @license  GNU General Public License
+ * @link     https://github.com/Efexis/ControlPanel/
+ * @version  0.0.1
+ */
+
+session_start();
+
+set_include_path(get_include_path()
+                    .PATH_SEPARATOR. dirname(__FILE__) . '/application/controllers'
+                    .PATH_SEPARATOR. dirname(__FILE__) . '/application/models'
+                    .PATH_SEPARATOR. dirname(__FILE__) . '/application/views'
+                    .PATH_SEPARATOR. dirname(__FILE__) . '/system/core'
+                    .PATH_SEPARATOR. dirname(__FILE__) . '/system/modules'
+);
+
+function __autoload($class) {
+    @include_once $class . '.php';
+}
+?>
