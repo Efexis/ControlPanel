bc506dadc292f79d19e479df9e45a1e17c8ae2f6
Efexis <efexlike@gmail.com>
2013-05-27 18:07:38 +0200

    Создаем контроллер IndexController и описываем ему конструктор

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Control Panel
+ *
+ * Copyright (c) 2013 Efex
+ *
+ * Панель управления для серверов WoW
+ *
+ * @author   Efex
+ * @license  GNU General Public License
+ * @link     https://github.com/Efexis/ControlPanel/
+ * @version  0.0.1
+ */
+
+class IndexController {
+    
+    private $model, $view;
+
+    public function __construct() {
+        $fc = FrontController::getInstance();
+        $this->model = new IndexModel();
+        $this->view = new View();
+    }
+}
+?>
