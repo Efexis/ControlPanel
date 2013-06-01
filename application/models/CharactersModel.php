4e9c370db8b88d62769811e95990d290594d07b6
Efexis <efexlike@gmail.com>
2013-06-01 16:14:57 +0200

    Создаем модель CharactersModel

---
+++ application/models/CharactersModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Control Panel
+ *
+ * Copyright (c) 2013 Efex
+ *
+ * Панель управления для серверов WoW
+ *
+ * @author   Efex
+ * @license  GNU General Public License
+ * @link     https://github.com/Efexis/ControlPanel/
+ * @version  0.0.1
+ */
+
+class CharactersModel extends Model {
+
+}
+?>
